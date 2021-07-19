e745cb537ede413aed4e05e9621e919be6297101
hasan-alkama <hasanalkamal786@gmail.com>
2021-07-19 20:29:22 +0530

    initial commit

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -24,28 +28,54 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the order button is clicked.
      */
-    int quantity=2;
+    int quantity = 2;
 
-    public void increment(View view){
-       quantity+=1;
+    public void increment(View view) {
+        quantity += 1;
         display(quantity);
     }
-    public void decrement(View view){
-        quantity-=1;
-        if(quantity>0)
-        display(quantity);
+
+    public void decrement(View view) {
+        quantity -= 1;
+        if (quantity > 0)
+            display(quantity);
         else {
-            quantity=1;
+            quantity = 1;
             display(1);
         }
     }
-    public void submitOrder(View view) {
-//        display(2);
-//        displayPrice(5*quantity);
-    String Message="the total price "+quantity*5+" \nthankyou";
-    displayMessage(Message);
+//    public int claculatePrice(int quantity2)
+//    {
+//        return 5*quantity2;
+//    }
+
+    public void submitOrder(View view)
+    {
+        EditText edt =(EditText) findViewById(R.id.edittxtview);
+        String val=edt.getText().toString();
+        CheckBox checkBoxwh=(CheckBox) findViewById(R.id.checkBox_whiped);
+        CheckBox checkBox1ch=(CheckBox) findViewById(R.id.checkBoxch);
+        boolean checkedwh=checkBoxwh.isChecked();
+        boolean checed1ch=checkBox1ch.isChecked();
+          int Price=CalculatePrice(checed1ch,checkedwh);
+         String body=createOrderSummary(Price,checkedwh,checed1ch,val);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+      intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, "hasanalkamal786@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for "+val);
+        intent.putExtra(intent.EXTRA_TEXT,body);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
+private int CalculatePrice(boolean checed1ch,boolean checkedwh)
+{
+    int price=5*quantity;
+    if(checed1ch)price+=2;
+    if(checkedwh)price+=1;
+    return price;
+}
     /**
      * This method displays the given quantity value on the screen.
      */
@@ -53,16 +83,24 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+//    private void displayPrice(int number) {
+//        TextView OrderSummaryTextView = (TextView) findViewById(R.id.OrderSummary_text_view);
+//       OrderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+//    }
+//
+//    private void displayMessage(String message) {
+//        TextView OrderSummaryTextView = (TextView) findViewById(R.id.OrderSummary_text_view);
+//        OrderSummaryTextView.setText(message);
+//    }
+
+    public String createOrderSummary(int price,boolean check,boolean ch1,String name)
+    {
+        String message = "NAME:"+name+"\nAdd whipedCream?"+check+ "\nAdd chocolate?"+ch1+"\nQUANTITY: " + quantity + "\nTOTAL PRICE: " + price+"\nTHANK YOU\nHAVE A GOOD DAY";
+        return message;
     }
 
 }
